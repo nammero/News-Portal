293874f88853d020dffb1f89322bd046732e37bf
greeflas <vldmr.kuprienko@gmail.com>
2019-03-22 18:48:58 +0200

    Generates URL to home page by route name

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
+    /**
+     * Renders site home page.
+     *
+     * @return Response
+     */
     public function index(): Response
     {
         return $this->render('default/index.html.twig');
