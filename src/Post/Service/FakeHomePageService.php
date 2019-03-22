cfebab56abce7ff55ec63b0c893b0d28dbf864a3
greeflas <vldmr.kuprienko@gmail.com>
2019-03-22 21:50:30 +0200

    Adds config parameter for posts limit

--- src/Post/Service/FakeHomePageService.php
+++ src/Post/Service/FakeHomePageService.php
@@ -4,15 +4,27 @@ namespace App\Post\Service;
 
 use App\Post\Collection;
 use App\Post\PostModel;
+use Psr\Log\LoggerInterface;
 
 final class FakeHomePageService implements HomePageServiceInterface
 {
+    private $logger;
+    private $postsLimit;
+
+    public function __construct(int $postsLimit, LoggerInterface $logger)
+    {
+        $this->postsLimit = $postsLimit;
+        $this->logger = $logger;
+    }
+
     public function getPosts(): Collection
     {
+        $this->logger->debug('Called fake home page service');
+
         $collection = new Collection();
         $faker = \Faker\Factory::create();
 
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < $this->postsLimit; $i++) {
             $post = new PostModel(
                 $faker->randomDigit,
                 $faker->randomElement(['World', 'Sport', 'IT', 'Science']),
